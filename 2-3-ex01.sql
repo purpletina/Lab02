e9f911fbaf1959eed861cc6d260f5f1436ab9154
purpletina <113041666+purpletina@users.noreply.github.com>
2023-09-24 14:09:41 +0800

    Add files via upload

---
+++ 2-3-ex01.sql
@@ -0,0 +1,6 @@
+CREATE TABLE Student
+(stu_id CHAR(5),
+stu_name NVARCHAR(10) Not Null,
+dep_code CHAR(4) Not Null,
+PRIMARY KEY(stu_id)
+)
---
+++ 2-4-4.sql
@@ -0,0 +1,2 @@
+INSERT INTO Student_bk (stu_id, stu_name, dep_code)
+SELECT stu_id, stu_name, dep_code FROM Student;
